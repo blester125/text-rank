c3646b6689ab6112d179307d81c30cbf5353e8c1
Brian <blester125@gmail.com>
2018-04-18 08:30:46 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+version = "0.0.1"
+
+setup(
+    name="text_rank",
+    version=version,
+    description="Text Rank with Cython",
+    author="Brian Lester",
+    author_email="blester125@gmail.com",
+    url="https://github.com/blester125/text_rank",
+    download_url=f"https://github.com/blester125/text_rank/archive/{version}.tar.gz",
+    license="MIT",
+    packages=find_packages(),
+)
---
+++ text_rank/__init__.py
@@ -0,0 +1,1 @@
+print("This is a placeholder to save the name")
